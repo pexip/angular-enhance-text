866ddbebc05dcc3b70e29eb2bbe304e7327e2964
Bernhard Posselt <dev@bernhard-posselt.com>
2014-02-10 01:35:48 +0100

    fix test

--- build/angular-enhance-text.js
+++ build/angular-enhance-text.js
@@ -3,32 +3,31 @@
 'use strict';
 
 
-var app = angular.module('bernhardposselt.enhancetext', []).
-    provider('enhanceTextFilter', function () {
-        var smilies = {};
+var app = angular.module('bernhardposselt.enhancetext', [])
+.provider('enhanceTextFilter', function () {
+    var smilies = {};
 
-        this.setSmilies = function (smiliesConfig) {
-            smilies = smiliesConfig;
+    this.setSmilies = function (smiliesConfig) {
+        smilies = smiliesConfig;
+    };
+
+    this.$get = function () {
+        return function (text) {
+
+            // loop over smilies and replace them in the text
+            var smileyKeys = Object.keys(smilies);
+            for(i=0; i<smileyKeys.length; i++) {
+                var smiley = smileyKeys[i];
+                var smileyKeyPath = smilies[smiley];
+                var replacement = '<img alt="' + smiley + '" src="' + 
+                    smileyKeyPath + '"/>';
+                
+                text = text.replace(smiley, replacement);
+            }
+
+            return text;
         };
-
-        this.$get = function () {
-            return function (text) {
-
-                // loop over smilies and replace them in the text
-                var smileyKeys = Object.keys(smilies);
-                for(i=0; i<smileyKeys.length; i++) {
-                    var smiley = smileyKeys[i];
-                    var smileyKeyPath = smilies[smiley];
-                    var replacement = '<img alt="' + smiley + '" src="' + 
-                        smileyKeyPath + '"/>';
-                    
-                    console.log(replacement);
-                    text.replace(smiley, replacement);
-                }
-
-                return text;
-            };
-        };
-    });
+    };
+});
 
 })(angular, undefined);
